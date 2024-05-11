3b2a2e75019d6133a00531a377315aa8586b2c58
BalaKeyanKarthiN <nbalakarthi97@gmail.com>
2024-05-11 12:55:38 +0530

    project intial setup

---
+++ src/test/java/com/example/vehicleapi/vehicle_search/VehicleSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vehicleapi.vehicle_search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
